f17cd312b577f90820b552de8f39f9064ce1aa12
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-15 18:03:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   phrase = "I LOVE YOU GRANDMA!"
   if phrase = phrase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase = phrase.capitalize
+  elsif phrase = phrase
   return "NO, NOT SINCE 1938!"
 else phrase = "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
