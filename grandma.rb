b13a1301f4d5bea8cc3f3c3e9434889f5539f718
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-15 17:43:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,4 @@
-def speak_to_grandma(phrase.upcase)
-  if phrase !== phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!",
-    elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+def speak_to_grandma(phrase)
 
 
 end
