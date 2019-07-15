6608e484c50e0e3fce4e7daad808dce119ba71bc
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-15 17:39:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,8 @@ def speak_to_grandma(phrase)
   if phrase !== phrase.upcase
     return "HUH?! SPEAK UP, SONNY!",
     elsif phrase == phrase.upcase
-    return ""
+    return "NO, NOT SINCE 1938!"
+  else 
     
 
 
